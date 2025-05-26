087721aea6404e14b8292e5ca4ff3048d5dd6f39
zalina1992 <maos8734@gmail.com>
2025-05-26 21:18:17 +0200

    rfreferfe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { BlogSingle2Component } from './blog-single2/blog-single2.component';
 import { BlogSingle3Component } from './blog-single3/blog-single3.component';
 import { BlogSingle4Component } from './blog-single4/blog-single4.component';
 import { BlogSingle5Component } from './blog-single5/blog-single5.component';
+import { BlogLayoutComponent } from './layout/blog-layout.component'; // <== DODAJ TO
 
 export const routes: Routes = [
   {
@@ -17,27 +18,32 @@ export const routes: Routes = [
         loadChildren: () =>
           import('./views/views.route').then((mod) => mod.VIEWS_ROUTE),
       },
-      {
-        path: 'sokemotoroptimalisering-effektivt-seo-oslo',
-        component: BlogSingleComponent,
-      },
-      {
-        path: 'seo-nettside-for-bedrifter',
-        component: BlogSingle2Component,
-      },
-      {
-        path: 'hvordan-bruke-sokeord-i-2025',
-        component: BlogSingle3Component,
-      },
-      {
-        path: 'seo-norge-2025',
-        component: BlogSingle4Component,
-      },
-      {
-        path: 'seo-sokemotoroptimalisering-effektivt',
-        component: BlogSingle5Component,
-      }
-    ]
+    ],
+  },
+  {
+    path: 'sokemotoroptimalisering-effektivt-seo-oslo',
+    component: BlogLayoutComponent,
+    children: [{ path: '', component: BlogSingleComponent }],
+  },
+  {
+    path: 'seo-nettside-for-bedrifter',
+    component: BlogLayoutComponent,
+    children: [{ path: '', component: BlogSingle2Component }],
+  },
+  {
+    path: 'hvordan-bruke-sokeord-i-2025',
+    component: BlogLayoutComponent,
+    children: [{ path: '', component: BlogSingle3Component }],
+  },
+  {
+    path: 'seo-norge-2025',
+    component: BlogLayoutComponent,
+    children: [{ path: '', component: BlogSingle4Component }],
+  },
+  {
+    path: 'seo-sokemotoroptimalisering-effektivt',
+    component: BlogLayoutComponent,
+    children: [{ path: '', component: BlogSingle5Component }],
   },
   {
     path: 'preview',
